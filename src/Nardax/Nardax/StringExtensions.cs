28e24b0fe70aae526b4c78eb68b17f06cb14222e
saksor23 <simon.lager@outlook.com>
2016-09-09 23:19:41 +0200

    Removed Turncate

--- src/Nardax/Nardax/StringExtensions.cs
+++ src/Nardax/Nardax/StringExtensions.cs
@@ -18,16 +18,5 @@ namespace Nardax
 
             return value.Substring(startIndex, maxLength);
         }
-
-        //Klipper en sträng från höger
-        public static string TruncateRight(this string value, int maxLength)
-        {
-            if (value.Length < maxLength)
-            {
-                return value;
-            }
-
-            return value.Substring(0, maxLength);
-        }
     }
 }
