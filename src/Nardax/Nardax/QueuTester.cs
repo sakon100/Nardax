760cbab765606fc88e7ba0e99115ea0e84134b2e
saksor23 <simon.lager@outlook.com>
2016-09-09 22:57:08 +0200

    Added File

---
+++ src/Nardax/Nardax/QueuTester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nardax
+{   
+    class QueuTester
+    {
+        
+
+
+        public QueuTester()
+        {
+            var _queue = new Queue<string>();
+            _queue.Enqueue("a");
+            _queue.Enqueue("b");
+            _queue.Enqueue("c");
+            _queue.Enqueue("d");
+            
+        }
+
+    }
+}
